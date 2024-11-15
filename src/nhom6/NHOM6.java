fab5b9849b3fd8fed37004078a8c2302740cf6ca
NguyenManhDung1906 <ngmanhdung1906@gmail.com>
2024-11-15 11:06:36 +0700

    Update NHOM6.java

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -9,41 +9,12 @@ package nhom6;
  * @author adminh
  */
 public class NHOM6 {
-    DBConnection dBConnection;
-     
-    public ArrayList<DanhMuc> getList(){
-        String sql = "select * from danh_muc dm" ;
-        ArrayList<DanhMuc> reslut = new ArrayList<>();
-        try(Connection conn = dBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql))
-        {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {                
-                Integer id = rs.getInt("id");
-                System.out.println("\nid la : "+id);
-                String ma_danh_muc = rs.getString("ma_danh_muc");
-                System.out.println("Ma la : "+ma_danh_muc);
-                String ten_danh_muc = rs.getString("ten_danh_muc");
-                System.out.println("Ten la : "+ten_danh_muc);
-                String trang_thai = rs.getString("trang_thai");
-                System.out.println("Trang thai la : "+trang_thai);
-                Date ngay_tao = rs.getDate("ngay_tao");
-                System.out.println("Ngay tao la : "+ngay_tao):
-                                  
-                Date ngay_sua = rs.getDate("ngay_tao");
-                System.out.println("Ngay sua la : "+ngay_sua);
-            }
-        }    
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return reslut;
-    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        aloo
     }
     
 }
