a01750d456a4aedb3d978e2e13dd5bdde90842ae
LeQuyBao1201 <baokutin2007@gmail.com>
2024-11-15 10:56:00 +0700

    dsfl;kds;lfjssd

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -15,6 +15,8 @@ public class NHOM6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.err.println("cytco");
+         
     }
     
 }
