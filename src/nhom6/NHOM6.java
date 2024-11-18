fbb200bdda8c1f2e7cb776bc4dd326f8e3d04afb
LeQuyBao1201 <baokutin2007@gmail.com>
2024-11-18 10:21:08 +0700

    commit

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -13,9 +13,46 @@ public class NHOM6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Nhom java 6"
-        );
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Chọn chức năng:");
+        System.out.println("1. Cộng");
+        System.out.println("2. Trừ");
+        System.out.println("3. Nhân");
+        System.out.println("4. Chia");
+        System.out.println("5. Nhập vào số nguyên và in ra các số lẻ");
+
+        System.out.print("Nhập lựa chọn của bạn (1-5): ");
+        int choice = scanner.nextInt();
+
+        if (choice >= 1 && choice <= 4) {
+            System.out.print("Nhập số thứ nhất: ");
+            int a = scanner.nextInt();
+            System.out.print("Nhập số thứ hai: ");
+            int b = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Kết quả: " + cong(a, b));
+                    break;
+                case 2:
+                    System.out.println("Kết quả: " + tru(a, b));
+                    break;
+                case 3:
+                    System.out.println("Kết quả: " + nhan(a, b));
+                    break;
+                case 4:
+                    System.out.println("Kết quả: " + chia(a, b));
+                    break;
+            }
+        } else if (choice == 5) {
+            System.out.print("Nhập một số nguyên: ");
+            int n = scanner.nextInt();
+            inSoLe(n);
+        } else {
+            System.out.println("Lựa chọn không hợp lệ!");
+        }
+
+        scanner.close();
     }
-    
 }
