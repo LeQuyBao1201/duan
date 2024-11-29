bd78ac05d71788403f6f9b5d6eeb71aac92976c6
LeQuyBao1201 <baokutin2007@gmail.com>
2024-11-29 10:32:59 +0700

    Update NHOM6.java

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -35,7 +35,7 @@ public static void main(String[] args) {
 
         int choice = scanner.nextInt();
 
-        if (choice >= 1 && choice <= 4) {
+        if (choice >= 1 && choice <= 11) {
             System.out.print("Nhap so thu nhat: ");
             int a = scanner.nextInt();
             System.out.print("Nhap so thu hai: ");
@@ -54,19 +54,19 @@ public static void main(String[] args) {
                 case 4:
                     System.out.println("Kết quả: " + chia(a, b));
                     break;
-                    case 5:
+                case 5:
                         System.out.println("nhập một số nguyên" );
                         int n = scanner.nextInt();
                         System.out.println("Các số lẻ :");
                         for (int i = 1; i <= n; i += 2) System.out.print(i + " ");
                     System.out.println();
                     break;
-                    case 6:
+                case 6:
                         scanner.nextInt();
                         Scanner.out.println("Nhập tên cần xóa");
                         System.out.println("Tên \""+ scanner.nextInt() + "\" đã được xóa (giả lập).");
                         break;
-                    case 7:
+                case 7:
                         System.exit(0);
                         default:
                             System.out.println("chon sai chuc nang vui long chon lai");
