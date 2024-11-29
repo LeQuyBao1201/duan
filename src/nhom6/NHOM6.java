2dc843daf2663632069e73ed6c99d71a24babf5f
LeQuyBao1201 <baokutin2007@gmail.com>
2024-11-29 07:42:48 +0700

    update

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -22,8 +22,8 @@ public static void main(String[] args) {
         System.out.println("3. Nhân");
         System.out.println("4. Chia");
         System.out.println("5. Nhập vào số nguyên và in ra các số lẻ");
-        System.out.print("asd");
         System.out.println("6. nhap vao so nguyen va in ra cac so chan");
+        System.out.println("6. nhap vao so nguyen va in ra cac so chia het cho 5");
         System.out.print("Nhập lựa chọn của bạn (1-5): ");
         int choice = scanner.nextInt();
 
