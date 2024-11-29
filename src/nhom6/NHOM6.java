a308890192f1a94d562aae6731cbe75822430118
NguyenManhDung1906 <ngmanhdung1906@gmail.com>
2024-11-29 10:15:15 +0700

    update them chuc nang 7

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -24,6 +24,7 @@ public static void main(String[] args) {
         System.out.println("5. Nhập vào số nguyên và in ra các số lẻ");
         System.out.println("6. nhap vao so nguyen va in ra cac so chan");
         System.out.println("6. nhap vao so nguyen va in ra cac so chia het cho 5");
+        System.out.println("6. nhap vao so nguyen va in ra cac so chia het cho 10");
         System.out.print("Nhập lựa chọn của bạn (1-5): ");
         int choice = scanner.nextInt();
 
