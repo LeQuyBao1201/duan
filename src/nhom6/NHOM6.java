d6adb7fa2e653c5fd4f1fd99e2a4fa312900b078
LeQuyBao1201 <baokutin2007@gmail.com>
2024-12-09 10:12:00 +0700

    up

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -35,6 +35,8 @@ public static void main(String[] args) {
         System.out.println("13. Nhập chiều dài chiều rộng tính chu vi");
         System.out.println("14. Nhập chiều dài chiều rộng tính dien tich");
         System.out.println("15. Nhập chiều dài chiều rộng chieu cao tính the tich");
+        System.out.println("16. Nhập chiều dài chiều rộng chieu cao tính the tich");
+       
         System.out.print("Nhập lựa chọn của bạn (1-20): ");
 
         int choice = scanner.nextInt();
