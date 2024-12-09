39706f106554803feae4ce6ae2977a2d3005265f
LeQuyBao1201 <baokutin2007@gmail.com>
2024-12-09 10:13:26 +0700

    new cn

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -35,7 +35,8 @@ public static void main(String[] args) {
         System.out.println("13. Nhập chiều dài chiều rộng tính chu vi");
         System.out.println("14. Nhập chiều dài chiều rộng tính dien tich");
         System.out.println("15. Nhập chiều dài chiều rộng chieu cao tính the tich");
-        System.out.println("16. Nhập chiều dài chiều rộng chieu cao tính the tich");
+        System.out.println("16. Nhập đường kính tính chu vi");
+        System.out.println("16. Có 3 lọ mất nhãn hãy phân biệt độ Ph");
        
         System.out.print("Nhập lựa chọn của bạn (1-20): ");
 
