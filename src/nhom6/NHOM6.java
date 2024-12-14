11c1f7115f72014aa73b28255f4f17e5869439dc
LeQuyBao1201 <baokutin2007@gmail.com>
2024-12-14 20:45:59 +0700

    up2

--- src/nhom6/NHOM6.java
+++ src/nhom6/NHOM6.java
@@ -41,6 +41,7 @@ public static void main(String[] args) {
         System.out.println("19. Nhập tên in ra địa chỉ nhà");
         System.out.println("20. InBody");
         System.out.println("21. InHealthy");
+        System.out.println("22. InTraining");
         System.out.println("20. Thoát");
         System.out.print("Nhập lựa chọn của bạn (1-20): ");
 
